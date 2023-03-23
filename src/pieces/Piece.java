783f3f1dc9e938580def68428ce1e6c561dc2ad0
Vedeka12 <vedeka12@gmail.com>
2023-03-23 22:13:48 +0530

    first commit

---
+++ src/pieces/Piece.java
@@ -0,0 +1,42 @@
+package pieces;
+
+import main.Board;
+
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+public class Piece {
+    public int col,row;
+    public int xPos, yPos;
+
+    public boolean isWhite;
+    public String name;
+    public int value;
+
+    public boolean isFirstMove = true;
+
+    BufferedImage sheet;
+    {
+        try{
+            sheet = ImageIO.read(ClassLoader.getSystemResourceAsStream("pieces.png"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }}
+    protected   int sheetScale = sheet.getWidth()/6;
+    Image sprite;
+    Board board;
+    public Piece(Board board){
+        this.board = board;
+    }
+
+    public boolean isValidMovement(int col, int row){
+        return true;}
+    public boolean moveCollidesWithPiece(int col, int row){
+        return false;}
+
+
+    public void paint(Graphics2D g2D){
+
+        g2D.drawImage(sprite, xPos, yPos, null);
+}}
